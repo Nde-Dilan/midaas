c8962a9a02b977380b0a08ab64687a8612feca79
MiltonJ23 <jonhmilton1991@gmail.com>
2026-06-15 22:22:24 +0100

    feat(notif): Notification service and email service definition contracts

---
+++ src/back/internal/contracts/notification.go
@@ -0,0 +1,29 @@
+package contracts
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type EmailMessage struct {
+	To          []string
+	Subject     string
+	HTML        string
+	Attachments []EmailAttachment
+}
+
+type EmailAttachment struct {
+	Filename    string
+	URL         string
+}
+
+type EmailService interface {
+	Send(ctx context.Context, msg EmailMessage) error
+}
+
+type NotificationService interface {
+	SendMilestoneApproved(ctx context.Context, projectID, milestoneID uuid.UUID, proofURLs []string) error
+	SendMilestoneRejected(ctx context.Context, projectID, milestoneID uuid.UUID, feedback string) error
+	SendRegistrationConfirmation(ctx context.Context, email, fullName string) error
+}
